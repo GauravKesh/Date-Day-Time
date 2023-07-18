d111bc7442d9440589c474462cc4f8a95fb36397
GauravKesh <gkrcoder@gmail.com>
2023-07-18 12:15:11 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+"debugger";
+const c = setInterval(myfunc,10);
+
+function myfunc(){
+  const dt = new Date();
+  let hours = dt.getHours();
+  let minute = dt.getMinutes();
+  let ap = hours >= 12 ? 'pm' : 'am';
+  const day=["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
+  const tday=day[dt.getDay()]
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let mont=months[dt.getMonth()];
+  document.querySelector(".date").value =dt.getDate() + "-" + mont + "-" + dt.getFullYear();
+  document.querySelector(".day").value =tday;
+  document.querySelector(".hour").value = dt.getHours();
+  document.querySelector(".minute").value = dt.getMinutes();
+  document.querySelector(".second").value = dt.getSeconds();
+  document.querySelector(".ampm").value = ap;
+
+
+}
